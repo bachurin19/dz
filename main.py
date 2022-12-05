100ae63440dcfd9a07fa49b28143610b8cdc650b
Mihail Bachurin <119751454+bachurin19@users.noreply.github.com>
2022-12-05 15:01:57 +0000

    feat:add 5 characteristics and add 6 functions & edit live function for different types

--- main.py
+++ main.py
@@ -1,140 +1,57 @@
-# print('Hello world')
-from random import *
+class Human():
+        def __init__(self, name):
+            self.name = name
+            self.age = 0
+            self.clever = 5
+        def live(self):
+            print(self.name, 'is alive')
+        def sleep(self):
+            print(self.name,'is sleeping')
+        def statistics(self):     
+            print('Name:',self.name,'Clever:',self.clever, 'Age:',self.age)
+            
+class Student(Human):
+    def __init__(self,name):
+        super().__init__(name)
+        self.grades=0
+    def live(self):
+        super().live()
+        print('What a great day at',self.name,'\'s school')
+    def studiyng(self):
+        print(self.name,'is studiyng')
+        self.grades+=2
+        self.clever+=10
+    def do_hm(self):
+        print(self.name,'is doing homework')
+        self.grades+=4
+        self.clever+=10
+class Teacher(Human):
+    def __init__(self,name):
+        super().__init__(name)
+        self.money=100
+        self.clever=100
+    def live(self):
+        super().live()
+        print('What a wonderful day of teaching,',self.name)
+    def teaching(self):
+        print(self.name,'is teaching')
+        self.money+=10
+        
+    def check_hm(self):
+        self.money+=10
+        print(self.name,'is checking homework')
+obj = Student('Bob')
+obj2 = Teacher('Clara')
+obj.live()
+obj2.live()
+obj2.teaching()
+obj.studiyng()
 
+obj.do_hm()
+obj2.check_hm()
+obj.sleep()
+obj2.sleep()
+obj.statistics()
+obj2.statistics()
 
-class University:
-    def __init__(self, title, faculty):
-        self.title = title
-        self.faculty = faculty
-        self.budget = False
-        self.student=True
-
-    def studying(self, name):
-        print(name + ' is studying')
-
-    def isbudget(self):
-        if self.budget == True:
-            print('Congratulations! You are on budget!')
-        else:
-            print('Pay money and study')
-
-
-class Teacher:
-    def __init__(self, name, subject):
-        self.name = name
-        self.subject = subject
-        self.mood = 0
-        self.grades = 0
-
-
-class Student:
-    def __init__(self, name):
-        self.name = name
-        self.gladness = 50
-        self.progress = 0
-        self.alive = True
-        self.money = 100
-
-    def ask_budget(self):
-        if self.progress > 30 or teacher.grades>15 and teacher.mood>10:
-            self.gladness += 10
-            univer.budget = True
-
-    def say_hello(self):
-        print('Hello!')
-
-    def work(self):
-        if univer.budget == False or self.money < 0:
-            print('Work')
-            self.money += 10
-            self.gladness -= 3
-            self.progress -= 1
-            teacher.grades-=2
-            teacher.mood-=1
-
-
-    def to_study(self):
-        if self.progress < 5:
-            self.gladness -= 4
-            self.progress += 5
-            self.gladness -= 1
-            teacher.grades+=5
-            teacher.mood+=5
-        if univer.budget == True:
-            self.gladness -= 1
-            print('Too much responsibility')
-        elif univer.budget == False:
-            self.money -= 5
-    print('Time to study')
-
-
-    def to_sleep(self):
-        print('Time to sleep')
-        self.gladness += 2
-
-
-    def to_chill(self):
-        print('Rest time')
-        self.gladness += 5
-        self.progress -= 2
-        self.money -= 5
-        teacher.mood-=1
-        teacher.grades-=2
-        univer.budget = False
-    def grades_check(self):
-        if teacher.grades<-50:
-            univer.student=False
-
-
-
-    def is_alive(self):
-        if self.progress < -10:
-            print('You are bad')
-            self.alive = False
-        elif self.gladness <= 0:
-            print('Dead inside')
-            self.alive = False
-        elif self.progress > 50:
-            print('Amazing! You are so smart')
-            self.alive = False
-        elif univer.student==False:
-            print('You are not student')
-            self.alive=False
-
-
-    def statics(self, univer):
-        print('Gladness: ', self.gladness, 'Progress: ', self.progress, 'Money:', self.money)
-        print('Budget: ', univer.budget,'Are you student?',univer.student)
-        print('Teacher mood:',teacher.mood,'Grades:',teacher.grades)
-
-
-    def live(self, day):
-        day = "Day " + str(day) + " of " + self.name + " life"
-        print(day)
-        self.grades_check()
-        live_cube = randint(1, 5)
-        if live_cube == 1:
-            self.to_study()
-            self.ask_budget()
-            univer.studying(self.name)
-        elif live_cube == 2:
-            self.to_sleep()
-        elif live_cube == 3:
-            self.to_chill()
-        elif live_cube == 4:
-            self.say_hello()
-        elif live_cube == 5:
-            self.work()
-        self.statics(univer)
-        self.is_alive()
-
-
-univer = University('Step', 'Computer Science')
-teacher = Teacher('Uchitel', 'Computer Science')
-sanya = Student('Sanya')
-for day in range(365):
-    if sanya.alive == False:
-        break
-    sanya.live(day)
-
-#
+    
